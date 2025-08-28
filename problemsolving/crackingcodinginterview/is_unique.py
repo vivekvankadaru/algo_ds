0e7a5c392c9fbdd03f7c089e3880ab7c46cf679c
vivekvankadaru <vivekvankadaru.fit@gmail.com>
2025-08-29 00:17:30 +0530

    commit1

---
+++ problemsolving/crackingcodinginterview/is_unique.py
@@ -0,0 +1,15 @@
+'''
+Implement an algorithm to determine if a string has all
+unique characters. What if you can't use any datastructures
+
+'''
+
+class is_unique():
+    def __init__(self, s):
+        self.s=s
+
+    def strategy1(self, s):
+        pass
+    
+if __name__=='__main__':
+    s=input('Enter the string')
